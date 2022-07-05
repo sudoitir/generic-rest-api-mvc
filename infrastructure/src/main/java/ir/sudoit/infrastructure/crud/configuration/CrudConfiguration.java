8553d6dede2ecc9a73c0b685b8e7d192f1405e2b
Mahdi Amirabdolahi <sudoit.ir@gmail.com>
2022-07-05 12:19:21 +0430

    [REFACTOR] Remove Configuration

--- infrastructure/src/main/java/ir/sudoit/infrastructure/crud/configuration/CrudConfiguration.java
+++
@@ -1,8 +0,0 @@
-package ir.sudoit.infrastructure.crud.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CrudConfiguration {
-
-}
--- infrastructure/src/main/java/ir/sudoit/infrastructure/person/configuration/PersonConfiguration.java
+++
@@ -1,7 +0,0 @@
-package ir.sudoit.infrastructure.person.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class PersonConfiguration {
-}
