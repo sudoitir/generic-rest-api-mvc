6fc5e16ae4119e6f55118aa810a358d43d083576
Mahdi Amirabdolahi <sudoit.ir@gmail.com>
2022-07-05 13:48:50 +0430

    [TEST] Create test for 'CrudUtilsTest', 'CallbackMapperTest' & 'BaseEntityTest'

---
+++ infrastructure/src/test/java/ir/sudoit/infrastructure/crud/persistence/model/BaseEntityTest.java
@@ -0,0 +1,87 @@
+package ir.sudoit.infrastructure.crud.persistence.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BaseEntityTest {
+    private Model x;
+    private Model y;
+    private Model z;
+
+    @BeforeEach
+    public void setUp() {
+        x = new Model(1);
+        y = new Model(1);
+        z = new Model(1);
+    }
+
+
+
+    @Test
+    void reflexive() {
+        assertThat(x).isEqualTo(x);
+    }
+
+    @Test
+    void symmetric() {
+        assertThat(x).isEqualTo(y);
+        assertThat(y).isEqualTo(x);
+    }
+
+    @Test
+    void transitive() {
+        assertThat(x).isEqualTo(y);
+        assertThat(y).isEqualTo(z);
+        assertThat(z).isEqualTo(x);
+    }
+
+    @Test
+    void consistence() {
+        for (int i = 0; i < 1000; i++) {
+            assertThat(x).isEqualTo(y);
+        }
+    }
+
+    @Test
+    void nullComparison() {
+        assertThat(new Model(null)).isNotNull();
+        assertThat(new Model(0)).isNotNull();
+        assertThat(new Model(Integer.MIN_VALUE)).isNotNull();
+        assertThat(new Model(Integer.MAX_VALUE)).isNotNull();
+    }
+
+    @Test
+    void hashCodeContract() {
+        assertThat(x.equals(y)).isTrue();
+        assertThat(x.hashCode()).isEqualTo(y.hashCode());
+
+        // consistence
+        assertThat(x.hashCode()).isEqualTo(x.hashCode());
+    }
+
+    @Test
+    void comparisonWithProxy() {
+        Model model = new Model(1);
+        Model proxy = (Model) Enhancer.create(
+                Model.class,
+                (MethodInterceptor) (object, method, args, methodProxy) -> methodProxy.invoke(model, args)
+        );
+        assertThat(model).isEqualTo(proxy);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Model extends BaseEntity<Integer> {
+        private Integer id;
+    }
+}
---
+++ infrastructure/src/test/java/ir/sudoit/infrastructure/crud/service/CallbackMapperTest.java
@@ -0,0 +1,32 @@
+package ir.sudoit.infrastructure.crud.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CallbackMapperTest {
+    private CallbackMapper<String, String> mapper;
+    private String source, target;
+
+    @BeforeEach
+    public void setUp() {
+        source = "source";
+        target = "target";
+        mapper = new CallbackMapper<>(this::map, this::callback);
+    }
+
+    @Test
+    void apply() {
+        assertThat(mapper.apply(source, target)).isEqualTo(this.source + "/" + this.target);
+    }
+
+    private String map(String source, String target) {
+        return source + "/" + target;
+    }
+
+    private void callback(String source, String target) {
+        assertThat(source).isEqualTo(this.source);
+        assertThat(target).isEqualTo(this.source + "/" + this.target);
+    }
+}
